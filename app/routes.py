d11422d289e7a84bd1b29c2639ad08ea1d1123a5
Nikita Antonenkov <krigar1184@gmail.com>
2018-07-21 22:24:53 +0300

    Database support in CI

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import asyncpg
 from sanic.response import json
 
 
-conn_string = f'postgresql://postgres@localhost:8001/postgres'
+conn_string = 'postgresql://postgres@localhost:8001/postgres'
 
 
 async def main(request):
